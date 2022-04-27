98e0a52905398516816ae271ca3b21328ab98bd4
Hoss <hoss@hardcoretech.co>
2022-04-27 18:49:54 +0800

    Support installing the package from Pip

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+Changelog
+=========
+
+0.1
+-----
+
+Changes:
+
+- Start this project by extracting the code from a private repository. 
---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="pyx12lib",
+    version="0.1",
+    author="CJHwong",
+    author_email="pypi@hardcoretech.co",
+    url="https://github.com/hardcoretech/pyx12-lib",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"pyx12lib": "pyx12lib"},
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "python-dateutil",
+        "six",
+    ],
+    python_requires=">=3.6",
+)
