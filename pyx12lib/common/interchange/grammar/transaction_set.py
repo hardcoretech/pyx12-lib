07a6db0d1edb49ee0b6348ebbea2c30389d7ec19
Hoss <hoss@hardcoretech.co>
2022-05-03 20:28:12 +0800

    refactor(common): replace strings by constants

--- pyx12lib/common/interchange/grammar/transaction_set.py
+++ pyx12lib/common/interchange/grammar/transaction_set.py
@@ -1,25 +1,24 @@
-from pyx12lib.core.grammar import Element
-from pyx12lib.core.grammar.segment import USAGE_MANDATORY, BaseSegment
+from pyx12lib.core.grammar import Element, BaseSegment, segment, element
 
 
 class StSegment(BaseSegment):
     segment_id = 'ST'
-    usage = 'M'
+    usage = segment.USAGE_MANDATORY
     max_use = 1
     elements = (
         Element(
             reference_designator='ST01',
             name='Transaction Set Identifier Code',
-            usage=USAGE_MANDATORY,
-            element_type='ID',
+            usage=element.USAGE_MANDATORY,
+            element_type=element.ELEMENT_TYPE_ID,
             minimum=3,
             maximum=3,
         ),
         Element(
             reference_designator='ST02',
             name='Transaction Set Control Number',
-            usage=USAGE_MANDATORY,
-            element_type='AN',
+            usage=element.USAGE_MANDATORY,
+            element_type=element.ELEMENT_TYPE_STRING,
             minimum=4,
             maximum=9,
         ),
@@ -33,16 +32,16 @@ class SeSegment(BaseSegment):
         Element(
             reference_designator='SE01',
             name='Number of Included Segments',
-            usage=USAGE_MANDATORY,
-            element_type='N0',
+            usage=element.USAGE_MANDATORY,
+            element_type=element.get_numeric_type(0),
             minimum=1,
             maximum=10,
         ),
         Element(
             reference_designator='SE02',
             name='Transaction Set Control Number',
-            usage=USAGE_MANDATORY,
-            element_type='AN',
+            usage=element.USAGE_MANDATORY,
+            element_type=element.ELEMENT_TYPE_STRING,
             minimum=4,
             maximum=9,
         ),
